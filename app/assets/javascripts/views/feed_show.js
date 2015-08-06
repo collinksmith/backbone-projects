4b0a7020a0e30d4e8eac9265818aeb1e44f5e92f
Collin Smith <collink33@gmail.com>
2015-08-06 14:43:19 -0700

    Add refresh button to feed show view

--- app/assets/javascripts/views/feed_show.js
+++ app/assets/javascripts/views/feed_show.js
@@ -1,6 +1,10 @@
 NewsReader.Views.FeedShow = Backbone.View.extend({
   template: JST['feed_show'],
 
+  events: {
+    "click .refresh": "reloadFeed"
+  },
+
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
   },
@@ -9,5 +13,11 @@ NewsReader.Views.FeedShow = Backbone.View.extend({
     var content = this.template({ feed: this.model });
     this.$el.html(content);
     return this;
+  },
+
+  reloadFeed: function () {
+    this.model.fetch({
+      success: this.render.bind(this)
+    });
   }
 });
